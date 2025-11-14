6d2446c5c85bc9e430c608d21648d09d88814426
rodgar00 <rodrigogarciaperez00@gmail.com>
2025-11-14 11:33:20 +0100

    Act

--- mainFlota.py
+++ mainFlota.py
@@ -3,10 +3,19 @@ def crear_tablero():
     for i in range (10):
         tablero.append([])
         for j in range (10):
-            tablero[i].append(0)
+            tablero[i].append(" ")
     return tablero
 
-def colocar_barcos(tablero):
+def colocar_barcos(tablero, m, m2, m3, m4):
+    print("Dime la posición a colocar entre paréntesis\n<< ")
+    for barco in range(4):
+        for acorazado in range(4):
+            coords = (int(input("Introduce la primera")), int(input("Introduce la siguiente")))
+            m[coords[0]][coords[1]] = "*"
+            m2[coords[0]][coords[1]] = "*"
+            m3[coords[0]][coords[1]] = "*"
+            m4[coords[0]][coords[1]] = "*"
+        return coords
     return tablero
 
 def printMatriz(tablero):
